e4b639cbc3d7ffa4168fc61862852da30c3753d9
Harish <121287864+Nagaha@users.noreply.github.com>
2023-07-07 10:07:10 +0530

    Update HackerRank_Easy_OutputFormat_5.java

--- HackerRank_Easy_OutputFormat_5.java
+++ HackerRank_Easy_OutputFormat_5.java
@@ -11,20 +11,20 @@ python         050
 ================================*/
 
 //This code formats the input in aspecific manner and it uses concepts of String,Logical operator.
-public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
             Scanner sc=new Scanner(System.in);
             System.out.println("================================");
-            char ch=' ';
             for(int i=0;i<3;i++){
                 String s1=sc.next();
                 int x=sc.nextInt();
-                
                 System.out.print(s1);
                 for(int y=s1.length()+1;y<=15;y++)
                  System.out.print(" ");
-                
-                if(x<100 && x!=0){
-                 
+                if(x<100 && x!=0){ 
                  System.out.println("0"+x); 
                 }
                 else if(x==0){
@@ -34,6 +34,6 @@ public static void main(String[] args) {
                  System.out.println(x);
             }
             System.out.println("================================");
-
+            sc.close();
     }
 }
