fc48b3730b1896627267c0c7b5102297c6e3d1b1
Harish <121287864+Nagaha@users.noreply.github.com>
2023-09-21 21:16:31 +0530

    returns the column number in excel sheet

--- Leetcode82Excel.java
+++ Leetcode82Excel.java
@@ -1,7 +1,7 @@
 class Solution {
     public int titleToNumber(String columnTitle) {
         int sum=0;
-      for(int i=0;i<columnTitle.length();i++){
+      for(int i=0;i<columnTitle.length();i++){ 
           sum=sum*26+(columnTitle.charAt(i)-'A'+1);
       }
       return sum;
